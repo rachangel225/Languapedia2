aa9fe724c1f76231805a193c1ba44b832fb8d136
Adeline <adelinesun@gmail.com>
2019-07-29 13:30:33 -0700

    added quiz

--- FProj_RachelEdits.js
+++ FProj_RachelEdits.js
@@ -24,25 +24,6 @@ function translateThis(){
   }
 }
 
-// function tabOpen(evt, cityName) {
-//   // Hide all elements with class="tabcontent" by default */
-//   var i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-//
-//   // Remove the background color of all tablinks/buttons
-//   tablinks = document.getElementsByClassName("tablink");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace("active", "");
-//   }
-//
-//   // Show the specific tab content
-//   document.getElementById(cityName).style.display = "block";
-//   evt.currentTarget.className += "active";
-
-
 
 function openPage(pageName, elmnt, color) {
   // Hide all elements with class="tabcontent" by default */
@@ -66,3 +47,49 @@ function openPage(pageName, elmnt, color) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+  $('#quiz-submit').on('click', function(){
+    //alert("yo");
+    var score = 0;
+
+    // Loop through each question
+    $('.question').each(function(){
+      // hide quiz msg
+      $('.quiz-msg', this).remove();
+
+      // find correct option
+      var correct = $(this).find(':checked[data-correct]').length;
+      //console.log(correct);
+
+      // check if correct or not
+      if( correct == 1){
+        // correct!
+        // add 1 point to the score
+        score++;
+        var msgHTML = '<div class="quiz-msg correct">Correct!</div>';
+        $(this).append(msgHTML);
+      }else{
+        // incorrect!
+        //alert("incorrect!");
+        var msgHTML = '<div class="quiz-msg incorrect">Incorrect!</div>';
+        $(this).append(msgHTML);
+      }
+
+    });
+
+    // Output score
+    $('#score').text(score);
+
+
+  });
+
+}); // end document ready
