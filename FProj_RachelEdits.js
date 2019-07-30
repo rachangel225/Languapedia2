af066d9276004a9d9a73e2befd4b8e6aa3065f8c
Adeline <adelinesun@gmail.com>
2019-07-30 16:06:07 -0700

    added a lot of content

--- FProj_RachelEdits.js
+++ FProj_RachelEdits.js
@@ -1,3 +1,21 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    }
+     else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
+
 function translateThis(){
 
   var text = document.getElementById("translate").value;
@@ -9,7 +27,8 @@ function translateThis(){
 
   translationRequest.send();
 
-  if(translationRequest.readyState != 4 || translationRequest.status != 200 || translationRequest.responseText ===''){
+  if(translationRequest.readyState != 4 || translationRequest.status != 200 || translationRequest.responseText ==='')
+  {
     window.console.error('Request had an error');
     return;
   }
