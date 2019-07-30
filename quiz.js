218de67216d5b61a9a316eca2a714c565651646a
Rachel Burak <rachelburak@yahoo.com>
2019-07-30 15:08:31 -0700

    background images

--- quiz.js
+++ quiz.js
@@ -68,7 +68,7 @@
   const submitButton = document.getElementById("submit");
   const myQuestions = [
     {
-      question: "How do you write 'I don't understand' in Mandarin?",
+      question: "1. How do you write 'I don't understand' in Mandarin?",
       answers: {
         a: "我五岁了",
         b: "再见",
@@ -77,23 +77,41 @@
       correctAnswer: "c"
     },
     {
-      question: "What is 'I' in Mandarin",
+      question: "2. What character means 'you' in Mandarin?",
       answers: {
         a: "我",
         b: "你",
         c: "不"
       },
-      correctAnswer: "a"
+      correctAnswer: "b"
     },
     {
-      question: "What is 'I'm sorry' in Mandarin?",
+      question: "3. What is 'I'm sorry' in Mandarin?",
       answers: {
         a: "对不起",
         b: "谢谢",
         c: "你好",
       },
       correctAnswer: "a"
-    }
+    },
+    {
+      question: "4. Which sentence means 'I love you'?",
+      answers: {
+        a: "你爱我",
+        b: "我爱你",
+        c: "爱",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "5. Use 'no' in a sentence.",
+      answers: {
+        a: "我不要去学校",
+        b: "我想你",
+        c: "她很美丽",
+      },
+      correctAnswer: "a"
+    },
   ];
 
   // display quiz right away
@@ -176,7 +194,7 @@
   const submitButton = document.getElementById("submit2");
   const myQuestions = [
     {
-      question: "What does '面条' mean in Mandarin?",
+      question: "1. What does '面条' mean in Mandarin?",
       answers: {
         a: "dumplings",
         b: "noodles",
@@ -185,7 +203,7 @@
       correctAnswer: "b"
     },
     {
-      question: "Which one of these is NOT a drink?",
+      question: "2. Which one of these is NOT a drink?",
       answers: {
         a: "包子",
         b: "奶茶",
@@ -194,14 +212,112 @@
       correctAnswer: "a"
     },
     {
-      question: "Which one of these is a type of '汤'？",
+      question: "3. Which one of these is a type of '汤'？",
       answers: {
         a: "Rice cakes",
         b: "Pork dumplings",
         c: "Hot and sour soup",
       },
       correctAnswer: "c"
-    }
+    },
+  ];
+
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
+
+
+
+(function musicquiz() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz3");
+  const resultsContainer = document.getElementById("results3");
+  const submitButton = document.getElementById("submit3");
+  const myQuestions = [
+    {
+      question: "1. What is 'music' in Mandarin?",
+      answers: {
+        a: "喜欢",
+        b: "电视剧",
+        c: "音乐",
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "2. What is 'TV shows' in Mandarin?",
+      answers: {
+        a: "喜欢",
+        b: "电视剧",
+        c: "音乐",
+      },
+      correctAnswer: "b"
+    },
   ];
 
   // display quiz right away
