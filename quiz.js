1a3aad40d129c8320494a156f382852cc408fa61
AsunAsdf <adelinesun@gmail.com>
2019-08-01 02:47:03 -0700

    fixed quiz

--- quiz.js
+++ quiz.js
@@ -1,328 +1,126 @@
-(function conversationquiz() {
-  function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
+$(document).ready(function(){
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
+  $('#quiz-submit').on('click', function(){
+    //alert("yo");
+    var score = 0;
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
+    // Loop through each question
+    $('.question').each(function(){
+      // hide quiz msg
+      $('.quiz-msg', this).remove();
+
+      // find correct option
+      var correct = $(this).find(':checked[data-correct]').length;
+      //console.log(correct);
+
+      // check if correct or not
+      if( correct == 1){
+        // correct!
+        // add 1 point to the score
+        score++;
+        var msgHTML = '<div class="quiz-msg correct">Correct!</div>';
+        $(this).append(msgHTML);
+      }else{
+        // incorrect!
+        //alert("incorrect!");
+        var msgHTML = '<div class="quiz-msg incorrect">Incorrect!</div>';
+        $(this).append(msgHTML);
       }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
     });
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+    // Output score
+    $('#score').text(score);
 
-  function showResults() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
-    let numCorrect = 0;
+  });
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
-
-        // color the answers green
-        answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
-
-  const quizContainer = document.getElementById("quiz");
-  const resultsContainer = document.getElementById("results");
-  const submitButton = document.getElementById("submit");
-  const myQuestions = [
-    {
-      question: "1. How do you write 'I don't understand' in Mandarin?",
-      answers: {
-        a: "我五岁了",
-        b: "再见",
-        c: "我听不懂"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "2. What character means 'you' in Mandarin?",
-      answers: {
-        a: "我",
-        b: "你",
-        c: "不"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "3. What is 'I'm sorry' in Mandarin?",
-      answers: {
-        a: "对不起",
-        b: "谢谢",
-        c: "你好",
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "4. Which sentence means 'I love you'?",
-      answers: {
-        a: "你爱我",
-        b: "我爱你",
-        c: "爱",
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "5. Use 'no' in a sentence.",
-      answers: {
-        a: "我不要去学校",
-        b: "我想你",
-        c: "她很美丽",
-      },
-      correctAnswer: "a"
-    },
-  ];
-
-  // display quiz right away
-  buildQuiz();
-
-  // on submit, show
-
-  submitButton.addEventListener("click", showResults);
-})();
+}); // end document ready
 
 
 
-(function foodquiz() {
-  function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
+$(document).ready(function(){
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
+  $('#quiz-submit2').on('click', function(){
+    //alert("yo");
+    var score2 = 0;
+
+    // Loop through each question
+    $('.question').each(function(){
+      // hide quiz msg
+      $('.quiz-msg', this).remove();
+
+      // find correct option
+      var correct = $(this).find(':checked[data-correct]').length;
+      //console.log(correct);
+
+      // check if correct or not
+      if( correct == 1){
+        // correct!
+        // add 1 point to the score
+        score2++;
+        var msgHTML = '<div class="quiz-msg correct">Correct!</div>';
+        $(this).append(msgHTML);
+      }else{
+        // incorrect!
+        //alert("incorrect!");
+        var msgHTML = '<div class="quiz-msg incorrect">Incorrect!</div>';
+        $(this).append(msgHTML);
       }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
     });
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+    // Output score
+    $('#score2').text(score2);
 
-  function showResults() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
-    let numCorrect = 0;
+  });
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
-
-        // color the answers green
-        answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
-
-  const quizContainer = document.getElementById("quiz2");
-  const resultsContainer = document.getElementById("results2");
-  const submitButton = document.getElementById("submit2");
-  const myQuestions = [
-    {
-      question: "1. What does '面条' mean in Mandarin?",
-      answers: {
-        a: "dumplings",
-        b: "noodles",
-        c: "milk tea"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "2. Which one of these is NOT a drink?",
-      answers: {
-        a: "包子",
-        b: "奶茶",
-        c: "茶"
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "3. Which one of these is a type of '汤'？",
-      answers: {
-        a: "Rice cakes",
-        b: "Pork dumplings",
-        c: "Hot and sour soup",
-      },
-      correctAnswer: "c"
-    },
-  ];
-
-  // display quiz right away
-  buildQuiz();
-
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
-})();
+}); // end document ready
 
 
 
-(function musicquiz() {
-  function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
+
+
+
+$(document).ready(function(){
+
+  $('#quiz-submit3').on('click', function(){
+    //alert("yo");
+    var score3 = 0;
+
+    // Loop through each question
+    $('.question').each(function(){
+      // hide quiz msg
+      $('.quiz-msg', this).remove();
+
+      // find correct option
+      var correct = $(this).find(':checked[data-correct]').length;
+      //console.log(correct);
+
+      // check if correct or not
+      if( correct == 1){
+        // correct!
+        // add 1 point to the score
+        score3++;
+        var msgHTML = '<div class="quiz-msg correct">Correct!</div>';
+        $(this).append(msgHTML);
+      }else{
+        // incorrect!
+        //alert("incorrect!");
+        var msgHTML = '<div class="quiz-msg incorrect">Incorrect!</div>';
+        $(this).append(msgHTML);
       }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
     });
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+    // Output score
+    $('#score3').text(score3);
 
-  function showResults() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
-    let numCorrect = 0;
+  });
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
-
-        // color the answers green
-        answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
-
-  const quizContainer = document.getElementById("quiz3");
-  const resultsContainer = document.getElementById("results3");
-  const submitButton = document.getElementById("submit3");
-  const myQuestions = [
-    {
-      question: "1. What is 'music' in Mandarin?",
-      answers: {
-        a: "喜欢",
-        b: "电视剧",
-        c: "音乐",
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "2. What is 'TV shows' in Mandarin?",
-      answers: {
-        a: "喜欢",
-        b: "电视剧",
-        c: "音乐",
-      },
-      correctAnswer: "b"
-    },
-  ];
-
-  // display quiz right away
-  buildQuiz();
-
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
-})();
+}); // end document ready
